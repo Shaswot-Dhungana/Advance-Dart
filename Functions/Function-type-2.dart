41142f1100299db225c871b7495c5efc70c143c2
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-11 21:39:52 +0545

    Create Function-type-2.dart

---
+++ Functions/Function-type-2.dart
@@ -0,0 +1,21 @@
+/*
+ Function With Argument and
+No Return Type
+*/
+
+void main() {
+// Calling Function
+
+addFunction(5,5);
+}
+
+
+
+// Function Declaration
+
+void addFunction(var num1 , var num2){
+var num3 = 0;
+num3 = num1 + num2 ;
+
+print("Sum of 2 Number is :~ $num3");
+}

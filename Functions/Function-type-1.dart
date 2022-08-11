16e649cc9e80a0ede3a85e08081e121930a42c12
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-11 21:37:46 +0545

    Create Function-type-1.dart

---
+++ Functions/Function-type-1.dart
@@ -0,0 +1,20 @@
+/* Function With No Argument
+And No Return Type.
+*/
+
+// Main Function
+
+void main() {
+
+// Calling Defined Function
+
+printText();
+
+}
+
+
+// Function Declaration ( User Defined Function )
+
+void printText() {
+print("This is a Demo Code of Ueer Defined Function With No Argument and Return Value/Type");
+}

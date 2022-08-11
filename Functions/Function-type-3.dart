e821eddf71683189409534083c3906aaa765687a
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-11 21:40:52 +0545

    Create Function-type-3.dart

---
+++ Functions/Function-type-3.dart
@@ -0,0 +1,28 @@
+// Function With No Argument and With Return Type
+
+
+// main Function
+
+
+void main() {
+
+print("First Number is 2.5");
+print("Second Number is 2.5");
+print("Their Sum is :~ ${funct()}");
+
+}
+
+
+
+// Function Declaration ( User Defined Function )
+
+double funct(){
+var num1,num2,num3;
+
+num1 = 2.5;
+num2 = 2.5;
+
+num3 = num1 + num2;
+
+return num3;
+}

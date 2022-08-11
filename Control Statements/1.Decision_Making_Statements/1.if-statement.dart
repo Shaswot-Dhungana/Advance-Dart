a5e072bfad6696387459396b0ee7e510680ec07d
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-11 22:10:53 +0545

    Add files via upload

---
+++ Control Statements/1.Decision_Making_Statements/1.if-statement.dart
@@ -0,0 +1,13 @@
+// main statement
+
+void main() {
+
+var a = 10;
+
+// If Statement 
+
+if(a>5) {
+print("10 is Greater Than 5");
+}
+
+}
---
+++ Control Statements/1.Decision_Making_Statements/2.if-Else-statement.dart
@@ -0,0 +1,23 @@
+
+
+
+// Main Function
+void main(){
+
+
+
+
+
+// if Else
+
+
+if(2>1){
+print("2 is greater than 1");
+}
+else{
+print("2 is smaller than 1");
+}
+
+
+}
+

b10cf1b1e6e4fbed6920c32d5ae67319a47969c7
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-12 15:35:40 +0545

    Update 1.While-Loop.dart

--- Control Statements/2.Dart Looping Statements/1.While-Loop.dart
+++ Control Statements/2.Dart Looping Statements/1.While-Loop.dart
@@ -1,1 +1,19 @@
-N
+// Main Function
+
+void main() {
+
+var a = 1;
+
+var b = 10;
+
+// While Loop
+
+while(a<b){   // it will print until the expression return false
+
+print("This is a Example of While Loop in Dart");
+
+a=a+1;     // increase value 1 after each iteration
+
+}
+
+}

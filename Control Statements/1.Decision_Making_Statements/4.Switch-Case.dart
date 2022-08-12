5230912f6ccadb6a01d129288fa75193b9d6af73
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-12 12:36:57 +0545

    Update 4.Switch-Case.dart

--- Control Statements/1.Decision_Making_Statements/4.Switch-Case.dart
+++ Control Statements/1.Decision_Making_Statements/4.Switch-Case.dart
@@ -1,1 +1,35 @@
-.
+// import Dart Input Output File
+
+import 'dart:io';                                    
+
+
+// main Function
+
+void main() {
+
+
+print("Enter Your Grade :~");
+
+// Scanning Grade
+
+String? grade = stdin.readLineSync();
+
+
+
+   switch(grade) {
+      case "A": {  print("Excellent"); }
+      break;
+
+      case "B": {  print("Good"); }
+      break;
+
+      case "C": {  print("Fair"); }
+      break;
+
+      case "D": {  print("Poor"); }
+      break;
+
+      default: { print("Invalid choice"); }
+      break;
+   }
+}

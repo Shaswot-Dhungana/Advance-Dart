0a3b01c1ff2253decdd2623e502a4bd870c6cd42
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-13 12:27:45 +0545

    Create Function-type-5.dart

---
+++ Functions/Function-type-5.dart
@@ -0,0 +1,22 @@
+// Main Function
+
+void main() {
+
+
+print(myMsg());
+
+print("This is Returned Value :~ ${numReturn()}");
+
+}
+
+
+
+// Anonymous Function                                   
+
+// Example 1
+
+ myMsg() => "This is an Example of Anonymous Function in Dart.";
+
+// Example 2 ( Anonymous Function with Integer Return Type.)
+
+int numReturn() => 123456;

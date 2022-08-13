d0f2abfac45b418d40cffdb4bd8dc2505aa3a341
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-13 21:33:29 +0545

    Update Continue_Statement.dart

--- Control Statements/Continue_Statement.dart
+++ Control Statements/Continue_Statement.dart
@@ -1,11 +1,29 @@
-Current Number is 0
-Current Number is 1
-Current Number is 2
-Current Number is 3
-Current Number is 4
-Current Number is 5
-Current Number is 6
-Current Number is 7
-Number 8 is Skipped (Due to Continue Statement)
-Current Number is 9
-Current Number is 10
+// Main Function
+
+void main()
+{
+
+
+// for loop
+
+
+
+for(int j=0 ; j<=10 ; j++) {
+
+
+// if statement
+
+if(j==8){
+
+print("Number 8 is Skipped (Due to Continue Statement)");
+
+continue;
+}
+
+
+print("Current Number is $j");
+
+}
+
+
+}

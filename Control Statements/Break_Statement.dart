b5db7e123227349a3424228a359cd491fb77d7c4
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-13 21:09:42 +0545

    Update Break_Statement.dart

--- Control Statements/Break_Statement.dart
+++ Control Statements/Break_Statement.dart
@@ -1,1 +1,29 @@
 
+// Main Function
+
+void main()
+{
+
+    int count = 1;
+
+// While Loop
+
+    while (count <= 10) {
+
+        print("Current Count Number is  $count");
+
+        count++;
+
+
+
+        if (count == 8) {
+print("Current Count Number is $count and Now Loop is Breaked.");
+
+            break;
+
+        }
+
+    }
+
+    print("Now you are out of while loop");
+}

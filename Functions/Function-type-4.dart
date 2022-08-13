85739e4c3ffae17c3d49452a4f42386289326aa4
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-13 12:14:46 +0545

    Update Function-type-4.dart

--- Functions/Function-type-4.dart
+++ Functions/Function-type-4.dart
@@ -1,1 +1,40 @@
-J
+import 'dart:io';
+
+
+// Main Function
+
+void main() {
+
+print("Enter First number:");
+
+// Reading User's Input From KeYBoard.
+// Scanning First number and storing it in Variable Named a.
+
+    int? num1 = int.parse(stdin.readLineSync()!);
+
+   // Here ? and ! are for null safety
+
+
+print("Enter Second number:");
+
+// Scanning Second number and storing it in Variable Named b.
+
+int? num2 = int.parse(stdin.readLineSync()!);
+
+
+// Function Calling Under Print Statement.
+
+print("Sum of Those 2 Numbers is :~ ${fun1(num1 , num2)}");
+
+}
+
+
+// Function With Argument and Return Type.
+// Declaring Function.
+
+int fun1(num1 , num2){
+
+int sum = num1 + num2;
+
+return sum;
+}

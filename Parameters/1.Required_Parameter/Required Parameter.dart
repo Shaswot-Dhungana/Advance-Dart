214f989231c33526c30723beb8199bd21e358c1d
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-14 07:27:36 +0545

    Update and rename NotEmpty to Required Parameter.dart

---
+++ Parameters/1.Required_Parameter/Required Parameter.dart
@@ -0,0 +1,17 @@
+// Main Function
+
+void main() {
+
+see("ParshuRam" , "Nepal");
+
+}
+
+
+// User-Defined Function
+// Example of Required Parameter.
+
+void see(String Name , String Address ){
+print("My Name is $Name");
+print("My Address is $Address");
+
+}I

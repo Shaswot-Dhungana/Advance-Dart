17cbf5c821e3329f6e9d8e69143c6e6d01223711
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-15 15:04:45 +0545

    Create Sound_Null_Safety.dart

---
+++ Sound_Null_Safety.dart
@@ -0,0 +1,27 @@
+// Main Function
+
+void main() {
+
+String? name = null; // ? makes Vairable Nullable
+print(name);
+
+name = "Nepal";
+print(name);
+
+
+int? age = null;
+print(age);
+
+
+age = 20;
+print(age);
+
+
+List<String?> address = ['Pokhara' , 'Mustang' , 'Kathmandu' , null ];
+print(address);
+
+
+List<String?>? addres = ['Pokhara' , 'Mustang' , 'Kathmandu' , null ];
+print(addres);
+
+}

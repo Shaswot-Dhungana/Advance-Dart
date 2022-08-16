81e76f049e6b02c9fdfa381cf3520609fad8e11d
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-16 14:46:59 +0545

    Update Function_As_A_ParaMeter.dart

--- Function_As_A_ParaMeter.dart
+++ Function_As_A_ParaMeter.dart
@@ -1,3 +1,25 @@
 // Function As A First Class Object.
 // Function Aa a Parameter.
 
+// Increasing Function
+int inc(int x) => ++x;
+
+// Decreasing Function
+int dec(int x) => --x;
+
+/* Using Increasing Function
+As a Parameter in The Below 'Apply' Function.
+/*
+
+int apply(int x, Function f) {
+  return f(x);
+}
+
+// Main Function
+
+void main() {
+  int r1 = apply(3, inc);
+  int r2 = apply(2, dec);
+  print(r1);
+  print(r2);
+}

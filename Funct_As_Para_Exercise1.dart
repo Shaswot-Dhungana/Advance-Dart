87f02b1dd8bd1a1bd4fd3249d4a83d0d0fc77741
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-08-17 16:02:47 +0545

    Create Funct_As_Para_Exercise1.dart

---
+++ Funct_As_Para_Exercise1.dart
@@ -0,0 +1,26 @@
+// Main Function
+
+void main(){
+
+// Calling Function dhg()
+
+ dhg('i Love ' , shasT);
+}
+
+
+
+// Declaring New Fumction
+
+String shasT(){
+const a = 'NEPAL';
+return a;
+}
+
+
+/* Passing Function shasT() As
+ a Parameter in
+Function dhg(). */
+
+void dhg(String a , Function k){
+print('$a ${shasT()} ');
+}
